343ac3ba73b247bd8e2b799ccab2a0b3e56ccf57
vivek verma <vivek459verma@gmail.com>
2025-12-29 23:47:26 +0530

    chnges in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
